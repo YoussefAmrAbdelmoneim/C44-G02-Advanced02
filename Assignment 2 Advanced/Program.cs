a97b646d307ee3118f94f587572e33bbbbbf82b6
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-25 22:18:27 +0300

    Q3

--- Assignment 2 Advanced/Program.cs
+++ Assignment 2 Advanced/Program.cs
@@ -2,6 +2,20 @@
 {
     internal class Program
     {
+        #region Q3
+        //public static void ReverseQueue(Queue<int> queue)
+        //{
+        //    Stack<int> stack = new Stack<int>();
+        //    while (queue.Count > 0)
+        //    {
+        //        stack.Push(queue.Dequeue());
+        //    }
+        //    while (stack.Count > 0)
+        //    {
+        //        queue.Enqueue(stack.Pop());
+        //    }
+        //}
+        #endregion
         static void Main(string[] args)
         {
             #region Q1
@@ -52,6 +66,19 @@
             //    Console.WriteLine("YES");
             //else
             //    Console.WriteLine("NO");
+            #endregion
+            #region Q3
+            //Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(1);
+            //queue.Enqueue(2);
+            //queue.Enqueue(3);
+            //queue.Enqueue(4);
+            //queue.Enqueue(5);
+            //ReverseQueue(queue);
+            //foreach (int item in queue)
+            //{
+            //    Console.Write($"{item} ");
+            //}
             #endregion
         }
     }
