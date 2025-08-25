6d2b10f581090b3fa96d6696a4b3536623e5635d
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-25 21:11:41 +0300

    Q1

--- Assignment 2 Advanced/Program.cs
+++ Assignment 2 Advanced/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Console.WriteLine("Please enter size of array");
+            //int.TryParse(Console.ReadLine(), out int size);
+            //int[] arr = new int[size];
+            //Console.WriteLine("Please enter number of Queries");
+            //int.TryParse(Console.ReadLine(), out int query);
+            //Console.WriteLine($"Array : ");
+            //for (int i = 0; i < arr?.Length; i++)
+            //{
+            //    int.TryParse(Console.ReadLine(), out arr[i]);
+            //}
+            //for (int i = 0; i < query; i++)
+            //{
+            //    Console.WriteLine($"Query {i+1}: ");
+            //    int.TryParse(Console.ReadLine(), out int x);
+            //    int count = 0;
+            //    for (int j = 0; j < arr?.Length; j++)
+            //    {
+            //        if (arr[j] > x)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine($"Numbers greater than {x} = {count}");
+            //}
+            #endregion    
         }
     }
 }
