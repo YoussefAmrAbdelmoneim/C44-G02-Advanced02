c2c36ea9f16c65a3476c483d62636f809ffdd459
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-25 22:06:17 +0300

    Q2

--- Assignment 2 Advanced/Program.cs
+++ Assignment 2 Advanced/Program.cs
@@ -29,7 +29,30 @@
             //    }
             //    Console.WriteLine($"Numbers greater than {x} = {count}");
             //}
-            #endregion    
+            #endregion
+            #region Q2
+            //Console.WriteLine("Please enter size of array: ");
+            //int.TryParse(Console.ReadLine(), out int size);
+            //int[] arr = new int[size];
+            //Console.WriteLine("Please enter items in array: ");
+            //for (int i = 0; i < arr?.Length; i++)
+            //{
+            //    int.TryParse(Console.ReadLine(), out arr[i]);
+            //}
+            //bool isPalindrome = true;
+            //for (int i = 0; i < size / 2; i++)
+            //{
+            //    if (arr?[i] != arr?[size - i - 1])
+            //    {
+            //        isPalindrome = false;
+            //        break;
+            //    }
+            //}
+            //if (isPalindrome)
+            //    Console.WriteLine("YES");
+            //else
+            //    Console.WriteLine("NO");
+            #endregion
         }
     }
 }
